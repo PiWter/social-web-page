92fd1cf611a928767dc9679381f06c818a4bc67c
PiWter <alfrevr05elpro@gmail.com>
2024-08-18 17:30:35 +0200

    personal-page

---
+++ script.js
@@ -0,0 +1,13 @@
+var socialWindow = document.getElementById('game');
+var tarjeta = document.getElementById('tarjeta');
+
+document.getElementById('tarjeta').addEventListener('click', function() {
+    game.style.animation = 'abrir .2s steps(60) 1 normal both'
+    game.style.display = 'block'
+    tarjeta.style.display = 'none'
+});
+
+document.getElementById('back-arrow').addEventListener('click', function() {
+    game.style.animation = 'cerrar .2s steps(60) 1 normal both'
+    tarjeta.style.display = 'grid'
+});
