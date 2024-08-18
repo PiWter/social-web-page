4d8f221e61dee3877540a89289cca1437889ed92
PiWter <alfrevr05elpro@gmail.com>
2024-08-18 18:11:02 +0200

    social

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-var socialWindow = document.getElementById('game');
+/*var socialWindow = document.getElementById('game');
 var tarjeta = document.getElementById('tarjeta');
-
 
 document.getElementById('tarjeta').addEventListener('click', function() {
     game.style.animation = 'abrir .2s steps(60) 1 normal both'
@@ -8,13 +7,7 @@ document.getElementById('tarjeta').addEventListener('click', function() {
     tarjeta.style.display = 'none'
 });
 
-/*document.getElementById('tarjeta').addEventListener('click', function() {
-    game.style.animation = 'abrir .2s steps(60) 1 normal both'
-    game.style.display = 'block'
-    tarjeta.style.display = 'none'
-});*/
-
 document.getElementById('back-arrow').addEventListener('click', function() {
     game.style.animation = 'cerrar .2s steps(60) 1 normal both'
     tarjeta.style.display = 'grid'
-});
+});*/
